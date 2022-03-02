4599fe02764eeb8c6a72f6a09844b562dc176477
John Waren <22johnwaren@mhs.usd266.com>
2022-03-02 13:39:23 -0600

    implement HTree draw function

--- src/HTree.java
+++ src/HTree.java
@@ -1,6 +1,15 @@
 public class HTree {
     public static void draw(int n, double length, double x, double y) {
-
+        if (n == 0) {
+            return;
+        }
+        StdDraw.line(x - length / 2, y, x + length / 2, y);
+        StdDraw.line(x - length / 2, y + length / 2, x - length / 2, y - length / 2);
+        StdDraw.line(x + length / 2, y + length / 2, x + length / 2, y - length / 2);
+        draw(n - 1, length / 2, x - length / 2, y + length / 2);
+        draw(n - 1, length / 2, x - length / 2, y - length / 2);
+        draw(n - 1, length / 2, x + length / 2, y + length / 2);
+        draw(n - 1, length / 2, x + length / 2, y - length / 2);
     }
 
     public static void main(String[] args) {
